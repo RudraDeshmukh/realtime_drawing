48b5ec28635d5e2f8e0c932c77ff5e42e4371c5e
RudraDeshmukh <99274112+RudraDeshmukh@users.noreply.github.com>
2022-06-21 20:55:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nosex=0;
+nosey=0;
+difference=0;
+leftwristX=0;
+rightwristX=0;
+
 function setup(){
     canvas = createCanvas(500,500)
     canvas.position(560,100)
@@ -12,6 +18,9 @@ function setup(){
 
 function draw(){
     background("#637187")
+    fill("#eb59ba")
+    stroke("#eb59ba")
+    square(nosex,nosey,difference)
 }
 
 function modelloaded(){
@@ -21,5 +30,15 @@ function modelloaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        nosex=results[0].pose.nose.x
+        nosey=results[0].pose.nose.y 
+        console.log("nosex = " + nosex + " nosey = " + nosey) 
+        leftwristX=results[0].pose.leftWrist.x
+        rightwristX=results[0].pose.rightWrist.x
+        difference = floor(leftwristX - rightwristX)
+        console.log(leftwristX,rightwristX)
+        document.getElementById("square_side").innerHTML="The width and height of the square are " + difference + "px"
+
+
     }
 }
